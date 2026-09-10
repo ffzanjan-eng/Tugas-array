1ed9f1e1b1dca665033e68e7156af59f07b1b0f2
Fakhri fauzan XIpplg2 <ffzanjan@gmail.com>
2026-09-10 09:18:34 +0700

    Add files via upload
    Tugas

---
+++ arrayassociative.php
@@ -0,0 +1,23 @@
+<?php
+
+    $datasiswa = [
+        "data_siswa_1" => "ahok", 
+        "hobi_siswa_1" => "masak,",
+        "data_siswa_2" => "wahyu",
+        "hobi_siswa_2" => "halu,",
+        "data_siswa_3" => "pahri",
+        "hobi_siswa_3" => "ngaji,",
+    ];
+
+    echo "Data nama siswa 1 " , $datasiswa["data_siswa_1"];
+    echo "<br/>";
+    echo "Hobi siswa 1 " , $datasiswa["hobi_siswa_1"];
+    echo "<br/>";
+    echo "Data nama siswa 2 " , $datasiswa["data_siswa_2"];
+    echo "<br/>";
+    echo "Hobi siswa 2 " , $datasiswa["hobi_siswa_2"];
+    echo "<br/>";
+    echo "Data nama siswa 3 " , $datasiswa["data_siswa_3"];
+    echo "<br/>";
+    echo "Hobi siswa 3 " , $datasiswa["hobi_siswa_3"];
+    echo "<br/>";
